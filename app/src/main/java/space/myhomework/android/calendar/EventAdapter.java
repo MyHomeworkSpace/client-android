070c6d538b5dc10ca7afa016a32d21108a9f8e99
Alex Studer <alex@studer.dev>
2023-10-20 22:49:22 -0400

    EventAdapter: show start and end time of events

--- app/src/main/java/space/myhomework/android/calendar/EventAdapter.java
+++ app/src/main/java/space/myhomework/android/calendar/EventAdapter.java
@@ -7,7 +7,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import space.myhomework.android.api.APIEvent;
 import space.myhomework.android.databinding.ItemEventBinding;
@@ -15,6 +18,8 @@ import space.myhomework.android.databinding.ItemEventBinding;
 public class EventAdapter extends RecyclerView.Adapter<EventViewHolder> {
     private Context context;
     private ArrayList<APIEvent> events;
+
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a", Locale.US);
 
     public EventAdapter(Context c, ArrayList<APIEvent> e) {
         context = c;
@@ -31,7 +36,16 @@ public class EventAdapter extends RecyclerView.Adapter<EventViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
         APIEvent event = events.get(position);
+
         holder.binding.eventName.setText(event.Name);
+
+        StringBuilder subtext = new StringBuilder();
+
+        subtext.append(timeFormat.format(new Date(event.Start * 1000L)));
+        subtext.append(" to ");
+        subtext.append(timeFormat.format(new Date(event.End * 1000L)));
+
+        holder.binding.eventSubtext.setText(subtext.toString());
     }
 
     @Override
