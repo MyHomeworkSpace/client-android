70d4ebd3c7789ca918d251a005397f4aa0df57cf
Alex Studer <alex@studer.dev>
2023-10-22 11:02:15 -0400

    api/APIEvent: make parcelable

--- app/src/main/java/space/myhomework/android/api/APIEvent.java
+++ app/src/main/java/space/myhomework/android/api/APIEvent.java
@@ -1,5 +1,8 @@
 package space.myhomework.android.api;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,7 +11,7 @@ import java.util.Iterator;
 
 import space.myhomework.android.calendar.EventTag;
 
-public class APIEvent {
+public class APIEvent implements Parcelable {
     public int ID;
     public String UniqueID;
     public String SeriesID;
@@ -40,4 +43,46 @@ public class APIEvent {
             Tags.put(tag, tagsObject.get(tagString));
         }
     }
+
+    public APIEvent(Parcel in) {
+        ID = in.readInt();
+        UniqueID = in.readString();
+        SeriesID = in.readString();
+        SeriesName = in.readString();
+        Name = in.readString();
+        Start = in.readInt();
+        StartTimezone = in.readString();
+        End = in.readInt();
+        EndTimezone = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(ID);
+        parcel.writeString(UniqueID);
+        parcel.writeString(SeriesID);
+        parcel.writeString(SeriesName);
+        parcel.writeString(Name);
+        parcel.writeInt(Start);
+        parcel.writeString(StartTimezone);
+        parcel.writeInt(End);
+        parcel.writeString(EndTimezone);
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        @Override
+        public APIEvent createFromParcel(Parcel parcel) {
+            return new APIEvent(parcel);
+        }
+
+        @Override
+        public APIEvent[] newArray(int i) {
+            return new APIEvent[i];
+        }
+    };
 }
